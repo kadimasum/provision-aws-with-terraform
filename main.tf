10c05538b5772c2a18da063e1836b4cf580dd734
kadimasum <kadimasam@gmail.com>
2024-01-08 13:06:42 +0300

    Add vpc resource and internet gateway resource

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+#1 create vpc
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+#2 Create internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
